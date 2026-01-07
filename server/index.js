4984933ac68bd9aae54d7d6160d778dd46c4ec87
RoyiKatz1 <royicats@gmail.com>
2026-01-07 18:49:24 +0200

    simple route to homepage

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Basic route
 app.get("/", (req, res) => {
-	res.json({ message: "Mixology Hub API is running!" });
+	res.sendFile(path.join(__dirname, "../client/pages/home.html"));
 });
 
 // Health check endpoint
